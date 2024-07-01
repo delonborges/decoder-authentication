4dd40b8e7d34d64e5598f757bd20f8e69a919655
Delon Borges <delon.borges@icloud.com>
2024-07-01 08:17:07 -0300

    refactor: remove userId from userDTO record

--- src/main/java/com/delon/decoderauthentication/dtos/UserDTO.java
+++ src/main/java/com/delon/decoderauthentication/dtos/UserDTO.java
@@ -2,11 +2,8 @@ package com.delon.decoderauthentication.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.util.UUID;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record UserDTO(UUID userId,
-                      String username,
+public record UserDTO(String username,
                       String email,
                       String password,
                       String oldPassword,
